3f2b90c6b02f2fcfd93427412cfc355136624cd0
Jon Chica <jchica72@gmail.com>
2018-05-07 22:22:28 +0200

    Greet new user and alert the others

--- server/server.js
+++ server/server.js
@@ -10,12 +10,27 @@ let server = http.createServer(app);
 let io = socketIO(server);
 io.on('connection', (socket) => {
   console.log('New user connected');
+  socket.emit('newMessage', {
+    from: 'Admin',
+    text: 'Welcome to the chat app.',
+    createdAt: new Date().getTime()
+  });
+  socket.broadcast.emit('newMessage', {
+    from: 'Admin',
+    text: 'New user in the chat.',
+    createdAt: new Date().getTime()
+  });
   socket.on('disconnect', () => {
     console.log('Client disconnected');
   });
   socket.on('createMessage', (message) => {
     console.log('New Message: ', message);
-    io.emit('newMessage', {
+    // io.emit('newMessage', {
+    //   from: message.from,
+    //   text: message.text,
+    //   createdAt: new Date().getTime()
+    // });
+    socket.broadcast.emit('newMessage', {
       from: message.from,
       text: message.text,
       createdAt: new Date().getTime()
